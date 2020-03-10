04c606f70e46bbcda5fa94f42db550916cdc1aa6
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-03-10 16:52:14 +0000

    Test for non numbers

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -2,12 +2,15 @@ describe("Calculator", function() {
     describe("Addition tests", function() {
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
-        })
+        });
         it("should return 26", function() {
             expect(addition(7, 19)).toBe(26);
-        })
+        });
         it("should return -8", function() {
             expect(addition(2, -10)).toBe(-8);
-        })
+        });
+        it("should return an error if we don't supply 2 numbers", function() {
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!!! That\'s not 2 bloody numbers");
+        });
     })
 })
