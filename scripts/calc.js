9466f116fa588d9c6cf2dd09e3361e9a11737a4a
Kevin <bourkekev@gmail.com>
2020-03-11 18:49:47 +0000

    End of Jasmine testing

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,15 @@
-function addition(numOne, numTwo) {
-    if ( typeof(numOne) == "number" && typeof(numTwo) == "number" ) {
-        return numOne+numTwo;
+function Calculator( value ) {
+    this.value = 0;
+}
+
+// constructor can also be written as:
+// Calculator = function() {
+//     this.value = 0;
+// };
+
+Calculator.prototype.add = function(number) {
+    if ( typeof(number) == "number" ) {
+        this.value += number;
     }
     else {
         alert( "Error!" );
